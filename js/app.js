4294efe685adf22a4dfafb16ad2e59dd363c9f2a
Joshua <olajidejoshua4real@gmail.com>
2019-06-18 00:12:17 +0100

    Clear Field method added

--- js/app.js
+++ js/app.js
@@ -41,11 +41,39 @@
             //Appending the row to the list
             list.appendChild(row);
         }
+
+         static clearFields(){
+            document.querySelector('#title').value = "";
+            document.querySelector('#author').value = "";
+            document.querySelector('#isbn').value = "";
+        }
     }
  //Store Class: Handle storage
 
  //Event: Display Books
     document.addEventListener('DOMContentLoaded', UI.displayBooks);
  //Event: Add a Book
+    document.querySelector('#book-form').addEventListener('submit', (e) =>
+    {   
+        // //Prevent actual default
+        e.preventDefault();
+        // // Get Form values
+        const title = document.querySelector('#title').value;
+        const author = document.querySelector('#author').value;
+        const isbn = document.querySelector('#isbn').value;
 
+        // Instantiate the Book
+        const book = new Book(title, author, isbn);
+        
+        UI.addBookToList(book);
+
+        // Clear Field on submit
+        UI.clearFields();
+
+        
+
+    })
  //Event: Remove a Book
+    document.querySelector('#book-list').addEventListener('click', (event) => {
+        console.log(event.target);
+    })
