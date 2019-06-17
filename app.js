979ea81cb3df51d25a774c3b1b936a6232aedea5
Joshua <olajidejoshua4real@gmail.com>
2019-06-17 23:10:28 +0100

    Book class Added in app.js file

---
+++ app.js
@@ -0,0 +1,18 @@
+//Book class: Represents a Book
+    class Book{
+        constructor(title, author, isbn) {
+            this.title = title;
+            this.author = author;
+            this.isbn = isbn;
+        }
+    }
+
+ //UI Class : Handle UI tasks
+
+ //Store Class: Handle storage
+
+ //Event: Display Books
+
+ //Event: Add a Book
+
+ //Event: Remove a Book
